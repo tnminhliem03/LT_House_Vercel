ceecb2f27a9f90cafaf6dd747d5d8a5164a15668
Minh Liêm <148978854+tnminhliem03@users.noreply.github.com>
2025-11-12 15:20:20 +0700

    Update API host URL in Api.js
    Change API host URL because change Pythonanywhere Account

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const HOST ="https://liemtnm03.pythonanywhere.com/";
+const HOST ="https://tnminhliem03.pythonanywhere.com/";
 // const HOST = "http://127.0.0.1:8000/"
 
 export const endpoints = {
