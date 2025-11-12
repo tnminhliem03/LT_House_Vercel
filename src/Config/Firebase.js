7579dd32278d3dafc1efa6a8245649a67d16ebc5
Minh Liêm <148978854+tnminhliem03@users.noreply.github.com>
2025-11-12 17:11:44 +0700

    Restore Firebase

--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -1,7 +1,20 @@
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-export const auth = getAuth();
-export const db = getFirestore();
-export const storage = getStorage();
+const firebaseConfig = {
+  apiKey: "AIzaSyBdSbRxRDxQk9ApXMpfscX_b5VP45-TJmU",
+  authDomain: "lt-house-4024d.firebaseapp.com",
+  projectId: "lt-house-4024d",
+  storageBucket: "lt-house-4024d.appspot.com",
+  messagingSenderId: "390395158173",
+  appId: "1:390395158173:web:6a566eced369e9faa0be74",
+  measurementId: "G-4JK6CV9C0N",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
